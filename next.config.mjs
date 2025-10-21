d70aeacd09e07f3e9ab5fc902ac26efdac32f68d
syahmi mokhtar <syahmiez96@gmail.com>
2025-10-21 21:58:53 +0800

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -17,4 +16,4 @@ const nextConfig = {
 
 };
 
-export default withFlowbiteReact(nextConfig);
+export default nextConfig;
